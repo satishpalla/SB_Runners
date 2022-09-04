c5d301bd4d5d6f2c77dc883395e6170a2ce3caf6
satish <spsatish.palla@gmail.com>
2022-09-04 11:37:11 +0530

    added Test5 class

---
+++ RunnersDemo/src/main/java/com/example/demo/Test5.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test5 {
+
+	
+public Test5() {
+
+System.out.println("Testb 5 class obj created");
+}
+	
+	
+}
